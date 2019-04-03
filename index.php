263c0d10bf880b2aafb461b0cc0eb5eac4e387b8
Corentin Foucrier <foucrier.corentin@gmail.com>
2019-04-03 12:19:46 +0200

    creation

--- index.php
+++ index.php
@@ -1,15 +1,45 @@
 <?php
 include('donnes.php');
+
+function ttc($prixttc){
+	$prixttc=$beerArray[$i][3]*1.2
+	return $prixttc;
+}
 ?>
+
+<! DOCTYPE html>
 <html>
 <head>
 	<title>Challenge semaine 4 - per programming</title>
 	<meta charset="utf-8" />
 </head>
 <body>
+	<h1>Les bières</h1>
 	<section>
+		<article>
+			<?php 
+			for($i=0;$i<count($beerArray) ;$i++){ 
+			?>
+				<!-- HEADER -->
+				<h3>
+					<?php
+					echo $beerArray[$i][0];
+					?>
+				</h3>
 
+				<!-- Image -->
+
+				<img src="<?= $beerArray[$i][1]?>" alt="photo de biere">
+				<p></p>		
+				<p></p>
+				
+			<?php
+			}
+			?>
+
+
+		</article>
 	</section>
-	<p></p>
+	
 </body>
 </html>
