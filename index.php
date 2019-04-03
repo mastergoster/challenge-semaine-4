4e9432453c714b0c2dbafade71776a24e386604c
Corentin Foucrier <foucrier.corentin@gmail.com>
2019-04-03 11:47:07 +0200

    creation

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+include('donnes.php');
+?>
+<html>
+<head>
+	<title>Challenge semaine 4 - per programming</title>
+	<meta charset="utf-8" />
+</head>
+<body>
+	<section>
+
+	</section>
+	<p></p>
+</body>
+</html>
