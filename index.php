6d4d830e480032687d9328f4a6010b923d7587d3
Corentin Foucrier <foucrier.corentin@gmail.com>
2019-04-03 15:03:26 +0200

    final release

--- index.php
+++ index.php
@@ -3,7 +3,8 @@ include('donnes.php');
 
 function ttc($prixht){
 	$prixttc=$prixht*1.2;
-	return $prixttc;
+	 /* Elle remplace les points par la virgule et elle arrondie à 2 décimales */
+	return number_format($prixttc,2,",", ".");
 }
 ?>
 
@@ -21,20 +22,20 @@ function ttc($prixht){
 			for($i=0;$i<count($beerArray) ;$i++){ 
 			?>
 				<!-- HEADER -->
-				<h3>
+				<h2>
 					<?php
 					echo $beerArray[$i][0];
 					?>
-				</h3>
+				</h2>
 
 				<!-- Image -->
 
 				<img src="<?= $beerArray[$i][1]?>" alt="photo de biere">
 				<!-- Description -->
-				<div><p><?= $beerArray[$i][2]?></p>	</div>	
+				<div><p><?= substr($beerArray[$i][2],0,150) . "[...]" ?></p>	</div>	
 				<!-- Prix -->
-				<div><p><?= ttc($beerArray[$i][3])?></p></div>
-
+				<div><p><?= ttc($beerArray[$i][3]) ." €" ?></p></div>
+<!--fonction str à voir et mettre en place $rest = substr("abcdef", 0, -1);
 			<?php
 			}
 			?>
